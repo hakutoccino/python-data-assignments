06729bed18d2c7c858daf32aa7d930c5aa3d29ac
hakutoccino <42828256+hakutoccino@users.noreply.github.com>
2018-10-23 17:07:03 +0800

    Add files via upload

---
+++ assignment0/TGIF.py
@@ -0,0 +1,12 @@
+print("hello from hakutoccino"）
+
+
+P = 10 ** 7
+r = 5 / 100 / 12
+n = 20 * 12
+A = P * r * (1 + r) ** n / ((1 + r) ** n - 1)
+
+print('P=', P)
+print('r=', r)
+print('n=', n)
+print('A=', A)
